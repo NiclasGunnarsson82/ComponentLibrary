44537c5452019aa08d17674eb6587a534c00d3c8
NiclasGunnarsson82 <71005102+NiclasGunnarsson82@users.noreply.github.com>
2026-07-04 15:58:19 +0200

    add ThemeContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Button } from "./components/Button";
+import { ThemeProvider } from "./utils/ThemeContext";
+
 const App = () => {
-  return <h1>Design System</h1>;
+  return(
+    <ThemeProvider>
+      <>
+        <h1>Design System</h1>
+        <Button
+            label="Send">
+        </Button>
+      </>
+    </ThemeProvider>
+
+    
+  );
 };
 
 export default App;
