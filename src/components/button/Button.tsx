c331dcf5542016e77d79ecc2e06ccc8571869cd9
NiclasGunnarsson82 <71005102+NiclasGunnarsson82@users.noreply.github.com>
2026-07-04 19:42:49 +0200

    add styles and scss

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,33 @@
+import { CSSProperties, ButtonHTMLAttributes } from "react";
+import { useTheme } from "@/utils/ThemeContext"
+import scss from "./Button.module.scss"
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    label: string
+}
+
+export const Button = ({
+    children, 
+    ...props }: ButtonProps) => {
+  
+    const { buttonColours } = useTheme()
+  
+    const styles = {
+        "--button-enabled": buttonColours.enabled,
+        "--button-hover": buttonColours.hover,
+        "--button-pressed": buttonColours.pressed,
+        "--button-focus": buttonColours.focus,
+        "--button-disabled": buttonColours.disabled,  
+    } as CSSProperties
+
+    return (
+        <button
+            disabled={true}
+            className={scss.customButton}
+            id={props.id}
+            aria-label={props["aria-label"]} 
+            style={styles}>
+            {children}
+        </button>
+    )
+}
