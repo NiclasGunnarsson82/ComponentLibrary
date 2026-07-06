ec205c6e779275896e947f5bf96e16d47b6c29dd
NiclasGunnarsson82 <71005102+NiclasGunnarsson82@users.noreply.github.com>
2026-07-06 13:20:20 +0200

    scss is dynamic based on optional isLoading prop

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -5,12 +5,14 @@ import scss from "./Button.module.scss"
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
     label: string,
     disabled?: boolean,
+    isLoading?: boolean,
     eventHandler: (event: MouseEvent<HTMLButtonElement>) => void
 }
 
 export const Button = ({
     label,
     disabled,
+    isLoading,
     eventHandler,
     children,
     ...props 
@@ -26,15 +28,27 @@ export const Button = ({
         "--button-colour-disabled": colours.blue400
     } as CSSProperties
 
+    //Handles disabled styling based on optional isLoading state
+    const className: string = isLoading === undefined 
+        ? scss.button 
+        : isLoading ? scss.isLoading : scss.button
+    //Handles optional disabled state
+    const isDisabled: boolean = disabled === undefined 
+        ? false 
+        : isLoading ? true : disabled
+
     return (
         <button
-            className={scss.button}
+            className={className}
             id={props.id}
             onClick={eventHandler}
-            disabled={disabled === undefined ? false : disabled}
+            disabled={isDisabled}
             aria-label={label} 
             style={styles}>
-                {label}
+                {/* Display loader if isLoading state is defined and set to true, else display label */}
+                {isLoading !== undefined && isLoading ?
+                    <span className={scss.loader}></span>
+                : label}
         </button>
     )
 }
