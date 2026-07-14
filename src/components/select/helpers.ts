c333032181dc40648e4da882b7ab8e40bdc5740a
NiclasGunnarsson82 <71005102+NiclasGunnarsson82@users.noreply.github.com>
2026-07-14 21:31:20 +0200

    options belong to optGroup or not

--- src/components/select/helpers.ts
+++ src/components/select/helpers.ts
@@ -6,7 +6,11 @@ export type SelectOptionType = {
     value: string,
     optGroup?: string
 }
-
+export type SelectOptionGroupType = {
+    group: string,
+    options: SelectOptionType[]
+}
+export type CompiledOptionsType = (SelectOptionGroupType | SelectOptionType)[]
 export type SelectProps = SelectHTMLAttributes<HTMLSelectElement> & {
     name: string,
     label: string,
@@ -18,4 +22,31 @@ export type SelectProps = SelectHTMLAttributes<HTMLSelectElement> & {
     selected?: SelectOptionType
 }
 
+export const compileOptions = (options: SelectOptionType[]): CompiledOptionsType => {
 
+    const optGroups: CompiledOptionsType = [];
+    
+    options.forEach((option) => {
+        //If optGroup is undefined
+        if (!option.optGroup) {
+            optGroups.unshift(option)
+            return
+        } 
+
+        //Does group property exist in object and is group already in the optGroups array?
+        const group = optGroups.find(g => "group" in g && g.group === option.optGroup)
+
+        //If group already exists in optGroup array, add current option
+        if (group && "options" in group) {
+            group.options.push(option)
+
+        //If groups does not exist, add new optGroup object with current option
+        } else {
+            optGroups.push({
+                group: option.optGroup,
+                options: [option]
+            })
+        }
+    })
+    return optGroups
+};
