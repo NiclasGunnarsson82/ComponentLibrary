ffce96a323999b81dfdc9c78b433b778a3a5aa3c
NiclasGunnarsson82 <71005102+NiclasGunnarsson82@users.noreply.github.com>
2026-07-15 13:51:51 +0200

    update shadows

--- src/data/designTokens.ts
+++ src/data/designTokens.ts
@@ -16,8 +16,8 @@ export const designTokens: IDesignTokens = {
         error:"#950606"        
     },
     shadows: {
-        buttonEnabled:"rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
-        buttonHover:"rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px",
+        buttonEnabled:"rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
+        buttonHover:"rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
     },
     font: {
         baseSize: "16px",
