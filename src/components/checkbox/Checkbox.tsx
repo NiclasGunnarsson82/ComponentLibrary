b1f10703c0be80a22c86e61ff1e62c3659029b20
NiclasGunnarsson82 <71005102+NiclasGunnarsson82@users.noreply.github.com>
2026-07-16 13:30:47 +0200

    add Checkbox styles

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -17,33 +17,32 @@ export const Checkbox = forwardRef<HTMLSelectElement, CheckboxProps>(
     const styles = {
         "--input-padding": form.inputPadding,
         "--input-shadow": form.inputShadow,
-        "--input-height": form.inputHeight,
+        "--checkbox-height": form.checkboxHeight,
+        "--checkbox-width": form.checkboxWidth,
         "--input-font-size": font.baseSize,
         "--font-family": font.fontfamily,
         "--font-weight": font.fontWeightRegular,
         "--input-border-radius": form.inputBorderRadius,
         "--input-colour-error": colours.error,
         "--input-colour-enabled": colours.blue300,
+        "--input-colour-active": colours.blue100,
     } as CSSProperties
 
     const checkboxClass: string =
-        state === "default" ? scss.select : scss.error
+        state === "default" ? scss.checkbox : scss.error
     
     return(
         <label 
             className={scss.label}
             style={styles} 
             htmlFor={name}>
-                <span>
-                    {label}
-                    {required && <strong> (required)</strong>}
-                </span>
-                <input
-                    className={checkboxClass}
+                <input 
+                    className={scss.checkbox}
                     {...props} 
                     type="checkbox" 
                     name={name} 
                     id={name} />
+            {label}                
         </label>       
     )
 })
