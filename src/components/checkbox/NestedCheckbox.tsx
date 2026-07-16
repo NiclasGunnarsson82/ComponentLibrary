92d1406e98659c5c4a0b8a6f380349754e8fea27
NiclasGunnarsson82 <71005102+NiclasGunnarsson82@users.noreply.github.com>
2026-07-16 19:29:48 +0200

    render nested checkboxes

--- src/components/checkbox/NestedCheckbox.tsx
+++ src/components/checkbox/NestedCheckbox.tsx
@@ -9,35 +9,63 @@ export const NestedCheckbox = forwardRef<HTMLSelectElement, NestedCheckboxProps>
         label,
         state,
         required,
-        checkboxes,
-        ...props }, ref) => {
+        checkboxes}, ref) => {
 
     const { colours, font, form } = useTheme()
 
     const styles = {
         "--input-padding": form.inputPadding,
         "--input-shadow": form.inputShadow,
-        "--input-height": form.inputHeight,
+        "--checkbox-height": form.checkboxHeight,
+        "--checkbox-width": form.checkboxWidth,
         "--input-font-size": font.baseSize,
         "--font-family": font.fontfamily,
         "--font-weight": font.fontWeightRegular,
         "--input-border-radius": form.inputBorderRadius,
         "--input-colour-error": colours.error,
         "--input-colour-enabled": colours.blue300,
+        "--input-colour-active": colours.blue100
     } as CSSProperties
 
     const checkboxClass: string =
         state === "default" ? scss.select : scss.error
+
+    const main: string = checkboxes.group + "-main"
     
     return(
-        <label 
-            className={scss.label}
-            style={styles} 
-            htmlFor={name}>
-                <span>
+        <div className={scss.checkboxList}>
+            <label 
+                className={scss.checkboxListLabel}
+                style={styles} 
+                htmlFor={name}>
+                    <input 
+                        className={scss.checkbox}
+                        style={styles}
+                        type="checkbox" 
+                        name={main} 
+                        value="all"
+                        id={main} />
                     {label}
-                    {required && <strong> (required)</strong>}
-                </span>
-        </label>            
+            </label>
+            <div className={scss.checkboxes}>
+                {checkboxes.checkboxes.map((checkbox) => {
+                    return(
+                        <label 
+                            className={scss.checkboxListLabel}
+                            style={styles} 
+                            htmlFor={name}>
+                            <input 
+                                className={scss.checkbox}
+                                style={styles}
+                                type="checkbox" 
+                                name={checkbox.option} 
+                                value={checkbox.value}
+                                id={name+checkbox.option}/>
+                                    {checkbox.option}
+                        </label> 
+                        )
+                    })}
+            </div>  
+       </div>              
     )
 })
