d11d62f0691467c78f0a6b6e953a6f40dde26b94
NiclasGunnarsson82 <71005102+NiclasGunnarsson82@users.noreply.github.com>
2026-07-22 16:12:48 +0200

    style input range and add currentValue state

--- src/components/range/Range.tsx
+++ src/components/range/Range.tsx
@@ -1,6 +1,6 @@
-import { CSSProperties, forwardRef } from "react";
+import { ChangeEvent, CSSProperties, forwardRef, useState } from "react";
 import { useTheme } from "@/utils/ThemeContext"
-import scss from "./Select.module.scss"
+import scss from "./Range.module.scss"
 import { RangeProps } from "./helpers";
 
 export const Range = forwardRef<HTMLInputElement, RangeProps>(
@@ -16,20 +16,27 @@ export const Range = forwardRef<HTMLInputElement, RangeProps>(
         step,
         ...props }, ref) => {
 
-    const { colours, font, form } = useTheme()
+    const { colours, font, form, slider } = useTheme()
 
     const styles = {
-        "--input-width": width ?? form.inputDefaultWidth,
-        "--input-padding": form.inputPadding,
         "--input-shadow": form.inputShadow,
-        "--input-height": form.inputHeight,
         "--input-font-size": font.baseSize,
         "--font-family": font.fontfamily,
         "--font-weight": font.fontWeightRegular,
-        "--input-border-radius": form.inputBorderRadius,
-        "--input-colour-error": colours.error,
-        "--input-colour-enabled": colours.blue300,
+        "--slider-track-height": slider.trackHeight,
+        "--slider-border-radius": slider.borderRadius,
+        "--slider-thumb-size": slider.thumbSize,
+        "--input-colour-default": colours.inputDefault,
+        "--thumb-colour-enabled": colours.blue300,
+        "--thumb-colour-focus": colours.blue200
     } as CSSProperties
+
+    const initial: string = (parseInt(max)/2).toString()
+    const [ currentValue, setCurrentValue ] = useState<string>(initial)
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setCurrentValue(event.target.value)
+        return
+    }
 
     return(
         <label 
@@ -40,15 +47,22 @@ export const Range = forwardRef<HTMLInputElement, RangeProps>(
                     {label}
                     {required && <strong> (required)</strong>}
                 </span>
-                 <input 
+                <input 
                     ref={ref}
+                    className={scss.slider}
+                    style={styles}
                     type="range" 
                     id={name} 
                     name={name} 
                     min="0" 
                     max="10"
                     step="1"
+                    onChange={handleChange}
+                    value={currentValue}
                     {...props}/>
+                <p style={{textAlign: "center"}}>
+                    {currentValue}
+                </p>
                 {errorMessage && state === "error" &&
                     <span className={scss.errorMessage}>
                         {errorMessage}
