86663375864bd7d7f1ccb18f295ce15257a8ce1a
NiclasGunnarsson82 <71005102+NiclasGunnarsson82@users.noreply.github.com>
2026-07-31 19:37:45 +0200

    remove colours from theme tokens

--- src/data/themes.ts
+++ src/data/themes.ts
@@ -1,20 +1,12 @@
 export type ThemeSelectorType = "light" | "dark" 
 
-export type ThemeColoursType = {
-    c100: string,
-    c200: string,
-    c300: string,
-    c400: string
-
-}
 export type ThemeType = {
     foreground: string,
     background: string,
     error: string,
     success: string,
     inputBorder: string,
-    inputBackground: string,
-    colours: ThemeColoursType
+    inputBackground: string
 }
 
 export interface IThemes {
@@ -28,13 +20,7 @@ export const themeTokens: IThemes = {
         error: "#950606",
         success: "#06402B",
         inputBorder: "#898989",
-        inputBackground: "#FFFFFF",
-        colours: {
-            c100:"#180c41",
-            c200:"#241161",
-            c300:"#301782",
-            c400:"#3c1da2"  
-        }
+        inputBackground: "#FFFFFF"
     },
     dark: {
         foreground: "#F8F8F8",
@@ -42,12 +28,6 @@ export const themeTokens: IThemes = {
         error: "#F85149",
         success: "#22C55E",
         inputBorder: "#898989",
-        inputBackground: "#1b1f23",
-        colours: {
-            c100:"#180c41",
-            c200:"#241161",
-            c300:"#301782",
-            c400:"#3c1da2"  
-        }
+        inputBackground: "#1b1f23"
     }    
 }
